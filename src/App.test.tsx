508ba2d21d716a2cb845d265e205e5cd43680ce2
Khoi <hakhoipham@gmail.com>
2020-05-02 15:02:26 -0400

    fixed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { StoreProvider } from './store';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(
+    <StoreProvider>
+      <App />
+    </StoreProvider>,
+  );
+  const linkElement = getByText(/Rick and Morty/i);
   expect(linkElement).toBeInTheDocument();
 });
