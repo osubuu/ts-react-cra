158f59805cd27c17bcee024f5d25e8b63e475cc7
Khoi <hakhoipham@gmail.com>
2020-05-02 16:20:56 -0400

    favorites page built

--- src/Favorites.tsx
+++ src/Favorites.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Store } from './store';
+import { toggleFavoriteAction } from './actions';
+import EpisodesList from './EpisodesList';
 
 export default function Favorites(): JSX.Element {
+  const { state, dispatch } = React.useContext(Store);
+
+  const episodeProps = {
+    episodes: state.episodes,
+    store: { state, dispatch },
+    toggleFavoriteAction,
+    favorites: state.favorites,
+  };
+
   return (
-    <div />
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <EpisodesList
+        episodes={episodeProps.favorites}
+        store={episodeProps.store}
+        toggleFavoriteAction={episodeProps.toggleFavoriteAction}
+        favorites={episodeProps.favorites}
+      />
+    </React.Suspense>
   );
 }
